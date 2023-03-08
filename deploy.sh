7fad320e11e1fc8a300b47c0621f4f7634dc30ee
fiqri khoirul m <fiqrikm18@gmail.com>
2023-03-08 10:12:19 +0700

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+npm run clean
+npm run build
+npm run tizen-deploy 
+npm run tizen-run
--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: 'build',
+  output: 'export',
+  // assetPrefix: ".",
+  images: {
+    unoptimized: true,
+  }
 }
 
 module.exports = nextConfig
